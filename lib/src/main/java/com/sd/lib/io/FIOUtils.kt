abe1248952e7aef199cedc8e23f69d66dd475b2e
zjun <565061763@qq.com>
2021-05-25 00:30:26 +0800

    FIOUtils

---
+++ lib/src/main/java/com/sd/lib/io/FIOUtils.kt
@@ -0,0 +1,43 @@
+package com.sd.lib.io
+
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+
+object FIOUtils {
+    /**
+     * 从[inputStream]中读取byte数组
+     */
+    @JvmStatic
+    @Throws(IOException::class)
+    fun readBytes(inputStream: InputStream): ByteArray {
+        return inputStream.use {
+            it.readBytes()
+        }
+    }
+
+    /**
+     * [inputStream]拷贝到[outputStream]
+     */
+    @JvmStatic
+    @JvmOverloads
+    @Throws(IOException::class)
+    fun copy(inputStream: InputStream, outputStream: OutputStream, callback: CopyCallback? = null): Long {
+        val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
+        var bytesCopied: Long = 0
+        var bytes = inputStream.read(buffer)
+        while (bytes >= 0) {
+            outputStream.write(buffer, 0, bytes)
+
+            bytesCopied += bytes
+            if (callback != null && callback.onBytesCopied(bytesCopied)) break
+
+            bytes = inputStream.read(buffer)
+        }
+        return bytesCopied
+    }
+
+    fun interface CopyCallback {
+        fun onBytesCopied(count: Long): Boolean
+    }
+}
