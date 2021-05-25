50e808b3ea9d2ff9c1b4b6f765ac7e7052e3f531
zjun <565061763@qq.com>
2021-05-25 09:21:15 +0800

    formatSize

--- lib/src/main/java/com/sd/lib/io/FFileUtils.kt
+++ lib/src/main/java/com/sd/lib/io/FFileUtils.kt
@@ -3,8 +3,13 @@ package com.sd.lib.io
 import android.content.Context
 import android.os.Environment
 import java.io.File
+import java.text.DecimalFormat
 
 object FFileUtils {
+    const val KB = 1024L
+    const val MB = 1024 * KB
+    const val GB = 1024 * MB
+
     /**
      * 外部存储是否存在
      */
@@ -159,13 +164,32 @@ object FFileUtils {
     }
 
     /**
-     * 返回[file]的大小
+     * 返回[file]的大小（字节）
      */
     @JvmStatic
     fun getSize(file: File?): Long {
         if (file == null || !file.exists()) return 0
         return file.walkBottomUp().fold(0) { acc, it ->
             acc + (if (it.isFile) it.length() else 0)
+        }
+    }
+
+    /**
+     * 返回格式化的字符串
+     */
+    @JvmStatic
+    @JvmOverloads
+    fun formatSize(byteSize: Long, df: DecimalFormat = DecimalFormat("#.00")): String {
+        return if (byteSize <= 0) {
+            "0.00B"
+        } else if (byteSize < KB) {
+            df.format(byteSize.toDouble()) + "B"
+        } else if (byteSize < MB) {
+            df.format(byteSize.toDouble() / KB) + "KB"
+        } else if (byteSize < GB) {
+            df.format(byteSize.toDouble() / MB) + "MB"
+        } else {
+            df.format(byteSize.toDouble() / GB) + "GB"
         }
     }
 }
