755e2d8b65a5f3a5c8f804444498ef95c0833c95
zjun <565061763@qq.com>
2021-05-25 15:24:24 +0800

    FUriUtils

--- lib/src/main/java/com/sd/lib/io/uri/FUriUtils.kt
+++ lib/src/main/java/com/sd/lib/io/uri/FUriUtils.kt
@@ -4,9 +4,13 @@ import android.content.Context
 import android.net.Uri
 import android.os.Build
 import androidx.core.content.FileProvider
+import androidx.documentfile.provider.DocumentFile
 import java.io.File
 
 object FUriUtils {
+    /**
+     * [File]转[Uri]
+     */
     @JvmStatic
     fun fileToUri(file: File, context: Context): Uri {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
@@ -14,5 +18,15 @@ object FUriUtils {
         } else {
             Uri.fromFile(file)
         }
+    }
+
+    /**
+     * [uri]文件名
+     */
+    @JvmStatic
+    fun getName(uri: Uri?, context: Context): String {
+        if (uri == null) return ""
+        val documentFile = DocumentFile.fromSingleUri(context, uri) ?: return ""
+        return documentFile.name ?: ""
     }
 }
