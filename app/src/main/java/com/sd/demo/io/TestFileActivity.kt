3b73c432bed9289e500e8f85be922e6e059d7928
zjun <565061763@qq.com>
2021-05-25 16:29:58 +0800

    demo

--- app/src/main/java/com/sd/demo/io/TestFileActivity.kt
+++ app/src/main/java/com/sd/demo/io/TestFileActivity.kt
@@ -1,10 +1,8 @@
 package com.sd.demo.io
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.sd.lib.io.FFileUtils
-import com.sd.lib.io.FTempDir
 import com.sd.lib.io.FZipUtils
 import java.io.File
 
@@ -25,31 +23,6 @@ class TestFileActivity : AppCompatActivity() {
         val cacheDir = FFileUtils.getCacheDir("my_cache", this)
         val filesDir = FFileUtils.getFilesDir("my_files", this)
 
-        // test copy file
-        val helloFile = File(cacheDir, "hello.txt").apply {
-            this.writeText("hello world!!!")
-        }
-        val copyFile = File(filesDir, "helloCopy.txt")
-        FFileUtils.copyFile(helloFile, copyFile)
-
-        // test move file
-        val moveFile = File(cacheDir, "move.txt").apply {
-            this.writeText("move!!!")
-        }
-        FFileUtils.moveFile(moveFile, File(filesDir, "moved.txt"))
-
-        // test copy dir
-        File(File(cacheDir, "deep"), "deep.txt").apply {
-            FFileUtils.checkParentDir(this)
-            this.writeText("deep content")
-        }
-
-        val cacheCopyDir = File(filesDir, "cacheCopy")
-        FFileUtils.copy(cacheDir, cacheCopyDir)
-        val cacheCopyDirSize = FFileUtils.getSize(cacheCopyDir)
-        Log.i(TAG, "cacheCopy size:${cacheCopyDirSize} format:${FFileUtils.formatSize(cacheCopyDirSize)}")
-
-        // test zip
         val cacheZip = File(filesDir, "cacheZip.zip")
         FZipUtils.zip(cacheDir, cacheZip)
         FZipUtils.unzip(cacheZip, File(filesDir, "cacheUnzip"))
@@ -62,6 +35,5 @@ class TestFileActivity : AppCompatActivity() {
 
         FFileUtils.delete(cacheDir)
         FFileUtils.delete(filesDir)
-        FTempDir.delete(this)
     }
 }
