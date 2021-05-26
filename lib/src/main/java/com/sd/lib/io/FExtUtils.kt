c15739a864a19bdb20adee56c8b5b7674da03612
zjun <565061763@qq.com>
2021-05-26 21:10:47 +0800

    getExt

--- lib/src/main/java/com/sd/lib/io/FExtUtils.kt
+++ lib/src/main/java/com/sd/lib/io/FExtUtils.kt
@@ -22,9 +22,7 @@ object FExtUtils {
         var ext = MimeTypeMap.getFileExtensionFromUrl(url)
         if (ext == null || ext.isEmpty()) {
             val lastIndex = url.lastIndexOf(".")
-            if (lastIndex > 0) {
-                ext = url.substring(lastIndex + 1)
-            }
+            if (lastIndex >= 0) ext = url.substring(lastIndex + 1)
         }
 
         return if (ext == null || ext.isEmpty()) {
