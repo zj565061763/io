19e9f88c3a7f630175255e2b388ac1eda860fd3a
zjun <565061763@qq.com>
2021-05-27 21:18:43 +0800

    FDirTemp

---
+++ lib/src/main/java/com/sd/lib/io/dir/FDirTemp.kt
@@ -0,0 +1,37 @@
+package com.sd.lib.io.dir
+
+import android.content.Context
+import com.sd.lib.io.FFileUtils
+import java.io.File
+
+/**
+ * 临时缓存目录
+ */
+object FDirTemp {
+    private val DIR by lazy { FDir("f_dir_temp") }
+
+    /**
+     * 返回目录
+     */
+    @JvmStatic
+    private fun get(context: Context): File? {
+        return DIR.get(context)
+    }
+
+    /**
+     * 删除目录
+     */
+    @JvmStatic
+    fun delete(context: Context) {
+        DIR.delete(context)
+    }
+
+    /**
+     * 创建文件
+     */
+    @JvmStatic
+    fun newFile(ext: String?, context: Context): File? {
+        val dir = get(context)
+        return FFileUtils.newFileUnderDir(dir, ext)
+    }
+}
