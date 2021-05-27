6697d1fb090378dd98bdd08513a66d05e3ddcfac
zjun <565061763@qq.com>
2021-05-27 21:49:25 +0800

    FDirUri

--- lib/src/main/java/com/sd/lib/io/dir/FDirUri.kt
+++ lib/src/main/java/com/sd/lib/io/dir/FDirUri.kt
@@ -24,6 +24,7 @@ object FDirUri {
      */
     @JvmStatic
     fun saveUri(uri: Uri?, context: Context): File? {
+        if (uri == null) return null
         val dir = DIR.get(context) ?: return null
         return DIR.lock {
             FUriUtils.saveToDir(uri, dir, context)
