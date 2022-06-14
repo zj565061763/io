edc281cfc5fa90a5516adb6f4f18379b99aab487
zhengjun <565061763@qq.com>
2022-06-15 03:51:30 +0800

    FDir

---
+++ lib/src/main/java/com/sd/lib/io/dir/FDir.kt
@@ -0,0 +1,59 @@
+package com.sd.lib.io.dir
+
+import android.content.Context
+import com.sd.lib.io.FFileUtils
+import java.io.File
+
+internal open class FDir(
+    dirName: String,
+    isCacheDir: Boolean = false,
+) {
+    private val _dirName = dirName
+    private val _isCacheDir = isCacheDir
+
+    init {
+        require(dirName.isNotEmpty()) { "dirName is empty" }
+    }
+
+    /**
+     * 返回目录
+     */
+    fun get(context: Context): File {
+        synchronized(_dirName) {
+            return if (_isCacheDir) {
+                FFileUtils.getCacheDir(_dirName, context)
+            } else {
+                FFileUtils.getFilesDir(_dirName, context)
+            }
+        }
+    }
+
+    /**
+     * 删除目录
+     */
+    fun delete(context: Context) {
+        synchronized(_dirName) {
+            val dir = get(context)
+            FFileUtils.delete(dir)
+        }
+    }
+
+    /**
+     * 创建文件
+     */
+    fun newFile(ext: String?, context: Context): File? {
+        synchronized(_dirName) {
+            val dir = get(context)
+            return FFileUtils.newFileUnderDir(dir, ext)
+        }
+    }
+
+    /**
+     * 同步操作
+     */
+    fun <R> lock(block: () -> R): R {
+        synchronized(_dirName) {
+            return block()
+        }
+    }
+}
