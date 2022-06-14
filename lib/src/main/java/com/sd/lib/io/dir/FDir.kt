f284cf1908e30a6a44221b6d47f54e37daf1e015
zhengjun <565061763@qq.com>
2022-06-15 03:45:41 +0800

    FDir

--- lib/src/main/java/com/sd/lib/io/dir/FDir.kt
+++ lib/src/main/java/com/sd/lib/io/dir/FDir.kt
@@ -4,19 +4,22 @@ import android.content.Context
 import com.sd.lib.io.FFileUtils
 import java.io.File
 
-class FDir(dir: String) {
-    private val _dir = dir
+/**
+ * 在[Context.getExternalFilesDir]下创建指定名称的目录，如果失败则尝试在[Context.getFilesDir]下创建
+ */
+class FDir(dirName: String) {
+    private val _dirName = dirName
 
     init {
-        require(dir.isNotEmpty()) { "dir is empty" }
+        require(dirName.isNotEmpty()) { "dirName is empty" }
     }
 
     /**
      * 返回目录
      */
     fun get(context: Context): File {
-        synchronized(_dir) {
-            return FFileUtils.getFilesDir(_dir, context)
+        synchronized(_dirName) {
+            return FFileUtils.getFilesDir(_dirName, context)
         }
     }
 
@@ -24,7 +27,7 @@ class FDir(dir: String) {
      * 删除目录
      */
     fun delete(context: Context) {
-        synchronized(_dir) {
+        synchronized(_dirName) {
             val dir = get(context)
             FFileUtils.delete(dir)
         }
@@ -34,7 +37,7 @@ class FDir(dir: String) {
      * 创建文件
      */
     fun newFile(ext: String?, context: Context): File? {
-        synchronized(_dir) {
+        synchronized(_dirName) {
             val dir = get(context)
             return FFileUtils.newFileUnderDir(dir, ext)
         }
@@ -44,7 +47,7 @@ class FDir(dir: String) {
      * 同步操作
      */
     fun <R> lock(block: () -> R): R {
-        synchronized(_dir) {
+        synchronized(_dirName) {
             return block()
         }
     }
