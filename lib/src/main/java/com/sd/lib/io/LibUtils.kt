e844df05378f82d4030756eaab41223ff9015191
zhengjun <565061763@qq.com>
2022-06-15 02:54:56 +0800

    md5

--- lib/src/main/java/com/sd/lib/io/LibUtils.kt
+++ lib/src/main/java/com/sd/lib/io/LibUtils.kt
@@ -5,18 +5,7 @@ import java.security.MessageDigest
 internal object LibUtils {
     @JvmStatic
     fun md5(value: String): String {
-        val bytes = MessageDigest.getInstance("MD5").apply {
-            this.update(value.toByteArray())
-        }.digest()
-
-        val builder = StringBuilder()
-        for (i in bytes.indices) {
-            val hex = Integer.toHexString(0xFF and bytes[i].toInt())
-            if (hex.length == 1) {
-                builder.append('0')
-            }
-            builder.append(hex)
-        }
-        return builder.toString()
+        val bytes = MessageDigest.getInstance("MD5").digest(value.toByteArray())
+        return bytes.joinToString("") { "%02X".format(it) }
     }
 }
