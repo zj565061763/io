e6c39a372388aa9217dbd116f3acf288ea12c00c
zhengjun <565061763@qq.com>
2022-06-15 05:28:58 +0800

    FFileProvider

--- lib/src/main/java/com/sd/lib/io/uri/FFileProvider.kt
+++ lib/src/main/java/com/sd/lib/io/uri/FFileProvider.kt
@@ -7,7 +7,7 @@ class FFileProvider : FileProvider() {
     companion object {
         @JvmStatic
         fun getAuthority(context: Context): String {
-            return "${context.packageName}.f-provider-lib-io"
+            return "${context.packageName}.f-f-provider-lib-io"
         }
     }
 }
