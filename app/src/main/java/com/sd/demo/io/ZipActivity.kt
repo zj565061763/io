90af685fa821c762d11ea39ef6c80364cf3a0596
zj565061763 <565061763@qq.com>
2023-05-27 12:48:20 +0800

    demo

--- app/src/main/java/com/sd/demo/io/ZipActivity.kt
+++ app/src/main/java/com/sd/demo/io/ZipActivity.kt
@@ -15,10 +15,10 @@ import androidx.compose.ui.unit.dp
 import com.sd.demo.io.ui.theme.AppTheme
 import com.sd.lib.io.FZipUtils
 import com.sd.lib.io.fCacheDir
+import com.sd.lib.io.fCheckDir
 import com.sd.lib.io.fDelete
 import com.sd.lib.io.fFilesDir
 import com.sd.lib.io.fZipTo
-import java.io.File
 
 class ZipActivity : ComponentActivity() {
 
@@ -39,12 +39,26 @@ class ZipActivity : ComponentActivity() {
     }
 
     private fun testZip() {
-        val cacheDir = fCacheDir("my_cache")
-        val filesDir = fFilesDir("my_files")
+        val cacheDir = fCacheDir("my_cache").apply {
+            this.resolve("testZip_hello.txt").apply {
+                this.writeText("hello")
+            }
+            this.resolve("testZip_world.txt").apply {
+                this.writeText("world")
+            }
+            this.resolve("testZip").apply {
+                this.fCheckDir()
+                this.resolve("hello world.txt").apply {
+                    this.writeText("hello world")
+                }
+            }
+        }
 
-        val cacheZip = File(filesDir, "cacheZip.zip")
-        cacheDir.fZipTo(cacheZip)
-        FZipUtils.unzip(cacheZip, filesDir)
+        val filesDir = fFilesDir("my_files")
+        val zip = filesDir.resolve("cacheZip.zip")
+
+        cacheDir.fZipTo(zip)
+        FZipUtils.unzip(zip, filesDir)
     }
 
     private fun deleteFile() {
