c28f2d4a3e30e438542c0be84f7a28c5ec114c8b
zj565061763 <565061763@qq.com>
2023-05-28 13:19:36 +0800

    FFileProvider

--- lib/src/main/java/com/sd/lib/io/uri/FFileProvider.kt
+++ lib/src/main/java/com/sd/lib/io/uri/FFileProvider.kt
@@ -3,7 +3,7 @@ package com.sd.lib.io.uri
 import androidx.core.content.FileProvider
 import com.sd.lib.ctx.fContext
 
-class FFileProvider : FileProvider()
+internal class FFileProvider : FileProvider()
 
 fun fFileProviderAuthority(): String {
     return "${fContext.packageName}.f-fp-lib-io"
