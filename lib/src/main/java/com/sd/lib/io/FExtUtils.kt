6d2cb91dfda1e8b87d80309d3f2954b2efbee334
zj565061763 <565061763@qq.com>
2023-05-30 15:58:53 +0800

    fExtRename

--- lib/src/main/java/com/sd/lib/io/FExtUtils.kt
+++ lib/src/main/java/com/sd/lib/io/FExtUtils.kt
@@ -5,6 +5,17 @@ import android.webkit.MimeTypeMap
 private const val Dot = "."
 
 /**
+ * 把当前字符串重命名为[name]，
+ * 如果[name]包含扩展名则返回[name]，
+ * 如果[name]不包含扩展名则返回[name].原扩展名
+ */
+fun String.fExtRename(name: String?): String {
+    if (name.isNullOrEmpty()) return this
+    if (name.fExt().isNotEmpty()) return name
+    return name + this.fExt().fExtAddDot()
+}
+
+/**
  * 获取扩展名，不包括"."（例如mp3），
  * 如果未获取到到扩展名，则返回[defaultExt]，如果[defaultExt]包括"."则会移除“.”后返回
  */
