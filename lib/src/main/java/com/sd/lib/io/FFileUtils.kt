63f2a379d5a16208fa9bf145bec25b5f47eb5824
zj565061763 <565061763@qq.com>
2023-06-02 14:57:35 +0800

    simplify

--- lib/src/main/java/com/sd/lib/io/FFileUtils.kt
+++ lib/src/main/java/com/sd/lib/io/FFileUtils.kt
@@ -41,21 +41,17 @@ fun fFilesDir(name: String? = null): File {
  * 在文件夹下创建一个扩展名为[ext]的文件
  */
 fun File?.fNewFile(ext: String): File? {
-    try {
-        if (this == null) return null
-        if (this.exists() && this.isFile) error("this file should not be a file")
-        val dotExt = ext.fExtAddDot()
-        while (true) {
-            val filename = UUID.randomUUID().toString() + dotExt
-            val file = this.resolve(filename)
-            if (file.exists()) {
-                continue
-            } else {
-                return file.takeIf { it.fCreateFile() }
-            }
+    if (this == null) return null
+    if (this.exists() && this.isFile) error("this file should not be a file")
+    val dotExt = ext.fExtAddDot()
+    while (true) {
+        val filename = UUID.randomUUID().toString() + dotExt
+        val file = this.resolve(filename)
+        if (file.exists()) {
+            continue
+        } else {
+            return file.takeIf { it.fCreateFile() }
         }
-    } catch (e: Exception) {
-        return e.libThrowOrReturn { null }
     }
 }
 
