eaff30c1dd8c8f207b2ef68587f6738cf9293f0a
zj565061763 <565061763@qq.com>
2023-08-30 14:35:13 +0800

    1.2.0-beta01

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "io"
-val libVersionName = "1.1.0-beta06"
+val libVersionName = "1.2.0-beta01"
 
 android {
     namespace = "com.sd.lib.io"
