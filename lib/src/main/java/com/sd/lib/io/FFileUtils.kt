0f475be5144ca2d2a102a9bbb246a5a2eb923b78
zjun <565061763@qq.com>
2024-03-08 12:41:29 +0800

    comment

--- lib/src/main/java/com/sd/lib/io/FFileUtils.kt
+++ lib/src/main/java/com/sd/lib/io/FFileUtils.kt
@@ -39,7 +39,8 @@ fun fFilesDir(name: String? = null): File {
 }
 
 /**
- * 在文件夹下创建一个扩展名为[ext]的文件
+ * 在当前[File]目录下创建一个扩展名为[ext]的文件并返回，
+ * 如果当前[File]是一个文件，则抛出异常[IllegalStateException]
  */
 fun File?.fNewFile(ext: String): File? {
     if (this == null) return null
