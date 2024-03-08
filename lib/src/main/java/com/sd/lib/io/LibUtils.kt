b5587e9853de65220642bcf1519fcaab46ee643d
zjun <565061763@qq.com>
2024-03-08 13:27:55 +0800

    libMD5

--- lib/src/main/java/com/sd/lib/io/LibUtils.kt
+++ lib/src/main/java/com/sd/lib/io/LibUtils.kt
@@ -3,6 +3,7 @@ package com.sd.lib.io
 import java.security.MessageDigest
 
 internal fun libMD5(value: String): String {
-    val bytes = MessageDigest.getInstance("MD5").digest(value.toByteArray())
-    return bytes.joinToString("") { "%02X".format(it) }
+    return MessageDigest.getInstance("MD5")
+        .digest(value.toByteArray())
+        .joinToString("") { "%02X".format(it) }
 }
